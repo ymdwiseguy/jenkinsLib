eb467f299e3d632ab5b6d0dca5744a8feb616a43
Matthias Diekmann <matthias.diekmann@rewe-digital.com>
2017-07-05 16:22:48 +0200

    modified build yarn step

--- vars/buildYarn.groovy
+++ vars/buildYarn.groovy
@@ -1,6 +1,6 @@
 def call(String packageVersion = '0') {
     wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
-        echo "\033[1;32mBuild yarn ${packageVersion} =="
+        echo "\033[1;32mBuild yarn"
         sh "GIT_HASH=`git rev-parse HEAD` && echo \"buildTestAndPackage() git hash: \"\$GIT_HASH"
         sh "cd ./src/main/frontend && yarn install && yarn test && GIT_HASH=`git rev-parse HEAD` yarn build"
     }
