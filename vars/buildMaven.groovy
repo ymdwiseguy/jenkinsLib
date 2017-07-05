f9f06e3ef18859c4e6ac1a26fbe8dbe91630bfea
Matthias Diekmann <matthias.diekmann@rewe-digital.com>
2017-07-05 16:16:49 +0200

    added build maven step

--- vars/buildMaven.groovy
+++ vars/buildMaven.groovy
@@ -1,5 +1,6 @@
 def call(String packageVersion = '0') {
     wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
+        echo "\033[1;32mBuild maven ${packageVersion} =="
         sh "MAVEN_COLOR=true mvn -Dmaven.multiModuleProjectDirectory=$WORKSPACE versions:set -DnewVersion=${packageVersion} -DgenerateBackupPoms=False"
         sh "MAVEN_COLOR=true mvn -Dmaven.multiModuleProjectDirectory=$WORKSPACE clean verify"
         sh "MAVEN_COLOR=true mvn -Dmaven.multiModuleProjectDirectory=$WORKSPACE install -DskipTests -Pbamboo"
