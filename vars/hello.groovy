9c8fd49a24016801c6ee7815f3a098707fd740c3
Matthias Diekmann <matthias.diekmann@rewe-digital.com>
2017-07-05 15:15:59 +0200

    added hello function

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,5 @@
 def sayHello() {
-    echo "\033[32m hello world from lib \033[0m"
+    wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
+        echo "\033[32m hello world from lib \033[0m"
+    }
 }
